bfa09f7231819c5c3718c7103a9e125326bc08d5
Melancia12345 <152531161+Melancia12345@users.noreply.github.com>
2024-08-28 17:36:10 -0300

    urna final

--- urna/clube.js
+++ urna/clube.js
@@ -4,17 +4,27 @@ function castVote(number) {
 }
  function validateFields() {
     const selectedNumber = document.getElementById('candidato-numero').value;
-    const mensagemErroElement = document.getElementById("mensagem-erro");
-    if (!selectedNumber) {
-        mensagemErroElement.innerText = "Por favor, aperte em um número para confirmar.";
-        mensagemErroElement.style.display = 'block';
-        setTimeout(() => {
-            mensagemErroElement.style.display = 'none';
-        }, 5000);
+   
+    var modal=document.querySelector("dialog");
+    var botal=document.querySelector("dialog button");
+    var modal3=document.querySelector("#erroConfirmar")
+    var botal3=document.querySelector("#erroConfirmar button ")
 
-} else {
-alert('Seu voto no número ' + selectedNumber + ' foi confirmado!');
-mensagenErrogeElement.style.display = 'none';
+    if (!selectedNumber) {
+       
+        modal3.showModal()
+
+        botal3.onclick=function(){
+            modal3.close()
+        }
+} 
+else {
+    modal.showModal()
+
+    botal.onclick=function(){
+        modal.close()
+    }
+
 }
 
 
@@ -22,16 +32,18 @@ mensagenErrogeElement.style.display = 'none';
 function deleteAction() {
     const inputField = document.getElementById('candidato-numero');
     const selectedNumber = inputField.value;
-    const mensagemErroElement = document.getElementById("mensagem-erro");
+    var modal5=document.querySelector("#modalapagar")
+    var botal5=document.querySelector("#modalapagar button")
+    
     if (!selectedNumber) {
-        mensagemErroElement.innerText = "Por favor, aperte um número para apagar.";
-        mensagemErroElement.style.display = 'block';
-        setTimeout(() => {
-            mensagemErroElement.style.display = 'none';
-        }, 5000);
+modal5.showModal()
+botal5.onclick=function(){
+    modal5.close()
+}
+       
     } else {
         inputField.value = selectedNumber.slice(0, -1);
-        mensagemErroElement.style.display = 'none';
+        
     }
 }
 
@@ -39,16 +51,25 @@ function deleteAction() {
 function deleteAll() {
     const inputField = document.getElementById('candidato-numero');
     const selectedNumber = inputField.value;
-    const mensagemErroElement = document.getElementById("mensagem-erro");
+    var modal4=document.querySelector("#modalexcluir")
+    var botao4=document.querySelector("#modalexcluir button")
+    
+    var moda2=document.querySelector("#Excluir");
+    var bota2=document.querySelector("#Excluir button");
+
     if (!selectedNumber) {
-        mensagemErroElement.innerText = "Por favor, aperte um número para excluir.";
-        mensagemErroElement.style.display = 'block';
-        setTimeout(() => {
-            mensagemErroElement.style.display = 'none';
-        }, 5000);
+        modal4.showModal()
+
+        botao4.onclick=function(){
+            modal4.close()
+        }
     } else {
-        alert('Seu voto no número ' + selectedNumber + ' foi cancelado!');
+        moda2.showModal()
+
+    bota2.onclick=function(){
+        moda2.close()
+    }
         inputField.value = '';
-        mensagemErroElement.style.display = 'none';
+       
     }
 }
