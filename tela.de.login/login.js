754e8b724707a1fc6ce4ed5cf7d66cafca8480b2
JIKLGJ <168481520+JIKLGJ@users.noreply.github.com>
2024-08-29 19:45:11 -0300

    login.js atualizado

--- tela.de.login/login.js
+++ tela.de.login/login.js
@@ -1,59 +1,63 @@
 
-function mostrarSenha() {
-  var inputPass  = document.getElementById('senha')
-  var bntShowpass =	document.getElementById('bnt-senha')
+var inputPass  = document.getElementById('senha');
+var bntShowpass = document.querySelector('#bnt-senha');
+var btnEntrar = document.querySelector('#Entrar')
 
-  if(inputPass.type === 'password'){
-      inputPass.setAttribute('type','text')
-      bntShowpass.classList.replace('bi bi-eye-fill','bi bi-eye-slash-fill')
-}else{
-  inputPass.setAttribute('type','password')
-      bntShowpass.classList.replace('bi bi-eye-slash-fill','bi bi-eye-fill')
-      
-      
+var botao = document.querySelector("#abrirModal")
+var modal = document.querySelector("#modal")
 
-}
+const ra = document.querySelector("#number1")
+const senha = document.querySelector("#senha")
+const okButton = document.querySelector('#okButton');
+
+
+botao.addEventListener('click', () => {
+  // Verifica se os campos estão vazios
+  if (ra.value != '000108453230' || senha.value != 'Al.2023#'){
+    
+    modal.showModal(); // Exibe o modal se algum campo estiver vazio
+  } else if  (ra.value === '' || senha.value === '')  {
+   
+    modal.showModal(); // Exibe o modal para indicar sucesso
+  } else {
+    
+    window.location.href = `http://127.0.0.1:5500/Pagina%20de%20bem%20vindo2/index.html`;
   }
+});
 
-  function validateFields() {
-      var num1 = document.getElementById("number1").value;
-      var senha = document.getElementById("senha").value;
-      var digito = document.getElementById("digito").value;
 
-      if (num1 === "" || senha === ""  ) {
-        document.getElementById("error-message").innerText =  
 
-       
-        Swal.fire({
-            icon: "error",
-            title: "Erro",
-            text: "assine os campos corretamente",
-            footer: '<a href="http://127.0.0.1:5500/site2/senha.html">Esqueceu a senha?</a>',
-          });
-      } else if (senha !== "Al.2023#" || num1 !== "000108453230")  {
-        document.getElementById("error-message").innerText =  
-
-       
-            Swal.fire({
-                icon: "error",
-                title: "Erro",
-                text: "Erro ao entrar na sua conta!",
-                footer: '<a href="http://127.0.0.1:5500/site2/senha.html">Esqueceu a senha?</a>'
-              });
-      } else {
-          window.location.href = `http://127.0.0.1:5500/Pagina%20de%20bem%20vindo2/index.html`;
-      }
-  }
-
-function maxlen(input){
-  if ( input.value.length >13){
-      input.value = input.value.slice(0,13);
-  }
-} 
 
 document.getElementById("number1").onkeypress = function(e) {
-  var chr = String.fromCharCode(e.which);
-  if ("1234567890".indexOf(chr) < 0)
-    return false;
+  var chr = String.fromCharCode(e.which );
+  if ("1234567890".indexOf(chr) < 0) {
+      e.preventDefault();
+  }
 };
 
+function mostrarSenha() {
+
+  if(inputPass.type === 'password'){
+      inputPass.setAttribute('type', 'text');
+      bntShowpass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill');
+      bntShowpass.style.opacity = 1;
+  } else {
+      inputPass.setAttribute('type', 'password');
+      bntShowpass.classList.replace('bi-eye-slash-fill', 'bi-eye-fill');
+      bntShowpass.style.opacity = 0.5;
+  }
+}
+  
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+  
+
+  okButton.addEventListener('click', () => {
+      modal.close(); // Fecha o modal
+  });
+
+  document.queryselector('#abrirModal').addEventListener('click', () => {
+      
+  });
+});
