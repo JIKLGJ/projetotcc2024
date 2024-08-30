bbe43345e644dde3ae73b1f4bd57c7bb4e75bed4
JIKLGJ <168481520+JIKLGJ@users.noreply.github.com>
2024-08-30 17:16:14 -0300

    login.js atualizado

--- tela.de.login/login.js
+++ tela.de.login/login.js
@@ -21,19 +21,13 @@ botao.addEventListener('click', () => {
     modal.showModal(); // Exibe o modal para indicar sucesso
   } else {
     
-    window.location.href = `http://127.0.0.1:5502/Pagina%20de%20bem%20vindo2/index.html`;
+    window.location.href = `http://127.0.0.1:5500/Pagina%20de%20bem%20vindo2/index.html`;
   }
 });
 
 
 
 
-document.getElementById("number1").onkeypress = function(e) {
-  var chr = String.fromCharCode(e.which );
-  if ("1234567890".indexOf(chr) < 0) {
-      e.preventDefault();
-  }
-};
 
 function mostrarSenha() {
 
@@ -61,3 +55,4 @@ document.addEventListener('DOMContentLoaded', () => {
       
   });
 });
+
